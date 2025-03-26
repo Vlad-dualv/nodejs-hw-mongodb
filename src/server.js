83cfcb719bd46fa63b678b2f2d2a0e29167f139c
Vlad <vladswork95@gmail.com>
2025-03-26 19:42:17 +0000

    Server running issue

--- src/server.js
+++ src/server.js
@@ -4,8 +4,6 @@ import cors from 'cors';
 
 import { getEnvVar } from './utils/getEnvVar.js';
 
-const PORT = Number(getEnvVar('PORT', '3000'));
-
 export const startServer = () => {
   const app = express();
 
@@ -26,7 +24,7 @@ export const startServer = () => {
     });
   });
 
-  app.use('*', (req, res, next) => {
+  app.use((req, res, next) => {
     res.status(404).json({
       message: 'Not found',
     });
@@ -38,6 +36,8 @@ export const startServer = () => {
       error: err.message,
     });
   });
+
+  const PORT = Number(getEnvVar('PORT')) || 3000;
 
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
