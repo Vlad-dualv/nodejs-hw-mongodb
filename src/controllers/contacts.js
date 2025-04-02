35e2c37a733a7aeb0a8e43efb2cf8c95917ad699
Vlad <vladswork95@gmail.com>
2025-04-02 19:15:17 +0100

    Update

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -43,8 +43,7 @@ export const deleteContactController = async (req, res, next) => {
   const { contactId } = req.params;
   const contact = await deleteContact(contactId);
   if (!contact) {
-    next(createHttpError(404, 'Contact not found'));
-    return;
+    throw createHttpError(404, 'Contact not found');
   }
   res.status(204).send();
 };
@@ -53,8 +52,7 @@ export const patchContactController = async (req, res, next) => {
   const { contactId } = req.params;
   const result = await updateContact(contactId, req.body);
   if (!result) {
-    next(createHttpError(404, 'Contact not found'));
-    return;
+    throw createHttpError(404, 'Contact not found');
   }
   res.json({
     status: 200,
